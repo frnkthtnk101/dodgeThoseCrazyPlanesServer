81eefb67a9c539bc31dfca0e535c24055257432f
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-29 18:10:09 -0400

    Update Main.py
    Intialized the server

--- Source/Main.py
+++ Source/Main.py
@@ -5,10 +5,16 @@ the dodgeThosCrazyPlanesServer
 '''
 
 import socket
-
  #set up the server
-
+HOST, PORT = '', 80
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
  #listen
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print('Dodge those crazy planes server Initialized')
+
+
 
  #figure out what to do
 
