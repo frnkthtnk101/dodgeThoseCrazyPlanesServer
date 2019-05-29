81ec858592f03971618d5dc974b0947421762169
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-29 19:21:59 -0400

    Update DTCPP_functionality.py
    put a vague discription

--- Source/DTCPP_functionality.py
+++ Source/DTCPP_functionality.py
@@ -5,14 +5,30 @@ all the functions in one convient package
 
 import json
 
+'''
+validates that the Json meets the PDU
+Standard.
+return a bool statement
+'''
 def validate_request(response):
     raise Exception("Not Implemented")
 
+'''
+initializes the game
+returns a response
+'''
 def initialize_game(response):
     raise Exception("Not Implemented")
 
+'''
+selects a level on file
+returns a respone
+'''
 def select_level(response):
     raise Exception("Not Implemented")
 
+'''
+ends the current game
+'''
 def end_game(response):
     raise Exception("Not Implemented")
