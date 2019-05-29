4c31e51a779f9cca9850e19a1cd378af9e32d55b
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-29 18:40:43 -0400

    Update Main.py
    ok, so this is the basic idea of the server. It will initialize and then serve with if then statements.

--- Source/Main.py
+++ Source/Main.py
@@ -5,6 +5,7 @@ the dodgeThosCrazyPlanesServer
 '''
 
 import socket
+import json
  #set up the server
 HOST, PORT = '', 80
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,8 +15,17 @@ listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print('Dodge those crazy planes server Initialized')
 
-
-
  #figure out what to do
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request = json.load(client_connection.recv(1024))
+    request_ok = validate_request(request)
+    if request_ok:
+        if int(request['message']) == 0:
+            respone = initialize_game(request)
+    client_connection.sendall(respone)
+    client_connection.close() 
 
+    print(request)
+    
  #close
