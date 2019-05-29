4b877d8a5665b36200588adc6060fd6e4244af42
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-29 19:02:32 -0400

    Update Main.py
    added the things to look for and comments where everthing happens

--- Source/Main.py
+++ Source/Main.py
@@ -20,10 +20,31 @@ while True:
     client_connection, client_address = listen_socket.accept()
     request = json.load(client_connection.recv(1024))
     request_ok = validate_request(request)
+    #at any of them it will session Id
+    #at any of them it will return error
     if request_ok:
+        #initialize game 
         if int(request['message']) == 0:
-            respone = initialize_game(request)
-    client_connection.sendall(respone)
+            #will send a cannot initialize game or
+            #Receive session ID/ Send Session ID
+            response = initialize_game(request)
+        #get level
+        elif int(request['message']) == 9:
+            #send a Receive level
+            response = select_level(request)
+        #bad level
+        elif int(request['message']) == 10:
+            document_bad_level(request)
+            response = select_level(request)
+        #quit game
+        elif int(request['message']) == 8:
+            #will send an OK
+            response = end_game(request)
+        #end game
+        elif int(request['message']) == 3:
+            #will send an OK
+            response = end_game(request)
+    client_connection.sendall(response)
     client_connection.close() 
 
     print(request)
