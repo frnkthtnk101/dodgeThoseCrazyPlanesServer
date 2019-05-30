aa418eda3e6738a9cb87defcd97a111630eda4ca
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-29 22:31:32 -0400

    Update Message_ids.py
    finish enum

--- Source/Classes/Message_ids.py
+++ Source/Classes/Message_ids.py
@@ -2,4 +2,15 @@ from enum import Enum
 
 class Message_ids(Enum):
     INTIALIZE_GAME = bytes(0)
+    CANNOT_INITIALIZE_GAME = bytes(1)
+    RECEIVE_SESSION_ID = bytes(2)
+    END_GAME = bytes(3)
+    QUIT_GAME = bytes(4)
+    OK = bytes(5)
+    SEND_SESSION_ID = bytes(6)
+    BAD_SESSION_ID = bytes(7)
+    GET_LEVEL = bytes(8)
+    RECEIVE_LEVEL = bytes(9)
+    BAD_LEVEL = bytes(10)
+    ERROR = bytes(11)
 
