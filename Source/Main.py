4d920804355cbe6820ecccc24d75eae267afdba6
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-29 21:20:41 -0400

    Update Main.py
    did a couple other things to the server

--- Source/Main.py
+++ Source/Main.py
@@ -8,16 +8,17 @@ import socket
 import json
 from DTCPP_functionality import *
 
- #set up the server
+#set up file system
+#set up the server
 HOST, PORT = '', 80
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
- #listen
+#listen
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print('Dodge those crazy planes server Initialized')
 
- #figure out what to do
+#figure out what to do
 while True:
     client_connection, client_address = listen_socket.accept()
     request = json.load(client_connection.recv(1024))
@@ -51,4 +52,4 @@ while True:
 
     print(request)
     
- #close
+#close
