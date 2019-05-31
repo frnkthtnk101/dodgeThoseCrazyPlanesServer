866d8ff1cbde7a436b4ba1ec03ab0ca019493fe7
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2019-05-31 10:38:31 -0400

    Update CreateData.ps1
    created 5 other levels

--- CreateData.ps1
+++ CreateData.ps1
@@ -1,45 +1,45 @@
-$Data1 = @{
+$easy1 = @{
     "Message" = 00001001;
     "Session ID" = 1;
     "Version" = 00001000;
     "Data" = @{
-        "Diffculty" = "Hard";
-        "PlaneTypes" = ("downers","lefters","righters");
+        "Diffculty" = "easy";
+        "PlaneTypes" = ("downers");
         "CompleteAt" = 1000;
         "level" = (
             @{"tick" = 10; "enemies" = "downers"; "number" =1},
-            @{"tick" = 20; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 30; "enemies" = "righters"; "number" =1},
+            @{"tick" = 20; "enemies" = "downers"; "number" =1},
+            @{"tick" = 30; "enemies" = "downers"; "number" =1},
             @{"tick" = 40; "enemies" = "downers"; "number" =1},
-            @{"tick" = 50; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 60; "enemies" = "righters"; "number" =1},
+            @{"tick" = 50; "enemies" = "downers"; "number" =1},
+            @{"tick" = 60; "enemies" = "downers"; "number" =1},
             @{"tick" = 70; "enemies" = "downers"; "number" =1},
-            @{"tick" = 80; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 90; "enemies" = "righters"; "number" =1},
+            @{"tick" = 80; "enemies" = "downers"; "number" =1},
+            @{"tick" = 90; "enemies" = "downers"; "number" =1},
             @{"tick" = 100; "enemies" = "downers"; "number" =1},
-            @{"tick" = 110; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 120; "enemies" = "righters"; "number" =1},
+            @{"tick" = 110; "enemies" = "downers"; "number" =1},
+            @{"tick" = 120; "enemies" = "downers"; "number" =1},
             @{"tick" = 130; "enemies" = "downers"; "number" =1},
-            @{"tick" = 140; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 150; "enemies" = "righters"; "number" =1},
+            @{"tick" = 140; "enemies" = "downers"; "number" =1},
+            @{"tick" = 150; "enemies" = "downers"; "number" =1},
             @{"tick" = 160; "enemies" = "downers"; "number" =1},
-            @{"tick" = 170; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 180; "enemies" = "righters"; "number" =1},
+            @{"tick" = 170; "enemies" = "downers"; "number" =1},
+            @{"tick" = 180; "enemies" = "downers"; "number" =1},
             @{"tick" = 190; "enemies" = "downers"; "number" =1},
-            @{"tick" = 200; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 300; "enemies" = "righters"; "number" =1},
+            @{"tick" = 200; "enemies" = "downers"; "number" =1},
+            @{"tick" = 300; "enemies" = "downers"; "number" =1},
             @{"tick" = 310; "enemies" = "downers"; "number" =1},
-            @{"tick" = 320; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 330; "enemies" = "righters"; "number" =1},
+            @{"tick" = 320; "enemies" = "downers"; "number" =1},
+            @{"tick" = 330; "enemies" = "downers"; "number" =1},
             @{"tick" = 400; "enemies" = "downers"; "number" =1},
-            @{"tick" = 500; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 600; "enemies" = "righters"; "number" =1},
+            @{"tick" = 500; "enemies" = "downers"; "number" =1},
+            @{"tick" = 600; "enemies" = "downers"; "number" =1},
             @{"tick" = 700; "enemies" = "downers"; "number" =1},
-            @{"tick" = 800; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 900; "enemies" = "righters"; "number" =1},
+            @{"tick" = 800; "enemies" = "downers"; "number" =1},
+            @{"tick" = 900; "enemies" = "downers"; "number" =1},
             @{"tick" = 901; "enemies" = "downers"; "number" =1},
-            @{"tick" = 901; "enemies" = "lefters"; "number" =1},
-            @{"tick" = 901; "enemies" = "righters"; "number" =1}
+            @{"tick" = 901; "enemies" = "downers"; "number" =1},
+            @{"tick" = 901; "enemies" = "downers"; "number" =1}
 
 
         )
@@ -47,6 +47,181 @@ $Data1 = @{
 
 }
 
+$easy2 = @{
+        "Diffculty" = "easy";
+        "PlaneTypes" = ("downers");
+        "CompleteAt" = 500;
+        "level" = (
+            @{"tick" = 10; "enemies" = "downers"; "number" =1},
+            @{"tick" = 20; "enemies" = "downers"; "number" =1},
+            @{"tick" = 30; "enemies" = "downers"; "number" =1},
+            @{"tick" = 40; "enemies" = "downers"; "number" =1},
+            @{"tick" = 50; "enemies" = "downers"; "number" =1},
+            @{"tick" = 60; "enemies" = "downers"; "number" =1},
+            @{"tick" = 70; "enemies" = "downers"; "number" =1},
+            @{"tick" = 80; "enemies" = "downers"; "number" =1},
+            @{"tick" = 90; "enemies" = "downers"; "number" =1},
+            @{"tick" = 100; "enemies" = "downers"; "number" =1},
+            @{"tick" = 110; "enemies" = "downers"; "number" =1},
+            @{"tick" = 120; "enemies" = "downers"; "number" =1},
+            @{"tick" = 130; "enemies" = "downers"; "number" =1},
+            @{"tick" = 140; "enemies" = "downers"; "number" =1},
+            @{"tick" = 150; "enemies" = "downers"; "number" =1},
+            @{"tick" = 160; "enemies" = "downers"; "number" =1},
+            @{"tick" = 170; "enemies" = "downers"; "number" =1},
+            @{"tick" = 180; "enemies" = "downers"; "number" =1},
+            @{"tick" = 190; "enemies" = "downers"; "number" =1},
+            @{"tick" = 200; "enemies" = "downers"; "number" =1},
+            @{"tick" = 300; "enemies" = "downers"; "number" =1},
+            @{"tick" = 310; "enemies" = "downers"; "number" =1},
+            @{"tick" = 320; "enemies" = "downers"; "number" =1},
+            @{"tick" = 330; "enemies" = "downers"; "number" =1},
+            @{"tick" = 400; "enemies" = "downers"; "number" =1},
+            @{"tick" = 500; "enemies" = "downers"; "number" =1}
+        )
+    }
+
+
+
+$easy3 =  @{
+        "Diffculty" = "easy";
+        "PlaneTypes" = ("downers");
+        "CompleteAt" = 650;
+        "level" = (
+            @{"tick" = 10; "enemies" = "downers"; "number" =2},
+            @{"tick" = 20; "enemies" = "downers"; "number" =1},
+            @{"tick" = 30; "enemies" = "downers"; "number" =1},
+            @{"tick" = 40; "enemies" = "downers"; "number" =1},
+            @{"tick" = 50; "enemies" = "downers"; "number" =1},
+            @{"tick" = 60; "enemies" = "downers"; "number" =1},
+            @{"tick" = 70; "enemies" = "downers"; "number" =1},
+            @{"tick" = 80; "enemies" = "downers"; "number" =1},
+            @{"tick" = 90; "enemies" = "downers"; "number" =1},
+            @{"tick" = 100; "enemies" = "downers"; "number" =1},
+            @{"tick" = 110; "enemies" = "downers"; "number" =1},
+            @{"tick" = 120; "enemies" = "downers"; "number" =1},
+            @{"tick" = 130; "enemies" = "downers"; "number" =1},
+            @{"tick" = 140; "enemies" = "downers"; "number" =1},
+            @{"tick" = 150; "enemies" = "downers"; "number" =1},
+            @{"tick" = 160; "enemies" = "downers"; "number" =1},
+            @{"tick" = 170; "enemies" = "downers"; "number" =1},
+            @{"tick" = 180; "enemies" = "downers"; "number" =1},
+            @{"tick" = 190; "enemies" = "downers"; "number" =1},
+            @{"tick" = 200; "enemies" = "downers"; "number" =1},
+            @{"tick" = 300; "enemies" = "downers"; "number" =1},
+            @{"tick" = 310; "enemies" = "downers"; "number" =1},
+            @{"tick" = 320; "enemies" = "downers"; "number" =1},
+            @{"tick" = 330; "enemies" = "downers"; "number" =1},
+            @{"tick" = 400; "enemies" = "downers"; "number" =1},
+            @{"tick" = 500; "enemies" = "downers"; "number" =1},
+            @{"tick" = 550; "enemies" = "downers"; "number" =2},
+            @{"tick" = 575; "enemies" = "downers"; "number" =2}
+        )
+    }
+
+$medium1 =  @{
+        "Diffculty" = "medium";
+        "PlaneTypes" = ("downers");
+        "CompleteAt" = 650;
+        "level" = (
+            @{"tick" = 10; "enemies" = "downers"; "number" =2},
+            @{"tick" = 20; "enemies" = "downers"; "number" =5},
+            @{"tick" = 30; "enemies" = "downers"; "number" =3},
+            @{"tick" = 40; "enemies" = "downers"; "number" =1},
+            @{"tick" = 50; "enemies" = "downers"; "number" =1},
+            @{"tick" = 60; "enemies" = "downers"; "number" =3},
+            @{"tick" = 70; "enemies" = "downers"; "number" =1},
+            @{"tick" = 80; "enemies" = "downers"; "number" =2},
+            @{"tick" = 90; "enemies" = "downers"; "number" =1},
+            @{"tick" = 100; "enemies" = "downers"; "number" =1},
+            @{"tick" = 110; "enemies" = "downers"; "number" =1},
+            @{"tick" = 120; "enemies" = "downers"; "number" =1},
+            @{"tick" = 130; "enemies" = "downers"; "number" =3},
+            @{"tick" = 140; "enemies" = "downers"; "number" =1},
+            @{"tick" = 150; "enemies" = "downers"; "number" =1},
+            @{"tick" = 160; "enemies" = "downers"; "number" =1},
+            @{"tick" = 170; "enemies" = "downers"; "number" =1},
+            @{"tick" = 180; "enemies" = "downers"; "number" =5},
+            @{"tick" = 190; "enemies" = "downers"; "number" =1},
+            @{"tick" = 200; "enemies" = "downers"; "number" =1},
+            @{"tick" = 300; "enemies" = "downers"; "number" =1},
+            @{"tick" = 310; "enemies" = "downers"; "number" =5},
+            @{"tick" = 320; "enemies" = "downers"; "number" =1},
+            @{"tick" = 330; "enemies" = "downers"; "number" =1},
+            @{"tick" = 400; "enemies" = "downers"; "number" =5},
+            @{"tick" = 500; "enemies" = "downers"; "number" =1},
+            @{"tick" = 550; "enemies" = "downers"; "number" =2},
+            @{"tick" = 575; "enemies" = "downers"; "number" =2}
+        )
+    }
+
+$medium2 =  @{
+    "Diffculty" = "medium";
+    "PlaneTypes" = ("downers");
+    "CompleteAt" = 500;
+    "level" = (
+        @{"tick" = 10; "enemies" = "downers"; "number" =2},
+        @{"tick" = 20; "enemies" = "downers"; "number" =5},
+        @{"tick" = 30; "enemies" = "downers"; "number" =3},
+        @{"tick" = 40; "enemies" = "downers"; "number" =1},
+        @{"tick" = 50; "enemies" = "downers"; "number" =1},
+        @{"tick" = 60; "enemies" = "downers"; "number" =3},
+        @{"tick" = 70; "enemies" = "downers"; "number" =1},
+        @{"tick" = 80; "enemies" = "downers"; "number" =2},
+        @{"tick" = 90; "enemies" = "downers"; "number" =1},
+        @{"tick" = 100; "enemies" = "downers"; "number" =1},
+        @{"tick" = 110; "enemies" = "downers"; "number" =1},
+        @{"tick" = 120; "enemies" = "downers"; "number" =1},
+        @{"tick" = 130; "enemies" = "downers"; "number" =3},
+        @{"tick" = 140; "enemies" = "downers"; "number" =1},
+        @{"tick" = 150; "enemies" = "downers"; "number" =1},
+        @{"tick" = 160; "enemies" = "downers"; "number" =1},
+        @{"tick" = 170; "enemies" = "downers"; "number" =1},
+        @{"tick" = 180; "enemies" = "downers"; "number" =5},
+        @{"tick" = 190; "enemies" = "downers"; "number" =1},
+        @{"tick" = 200; "enemies" = "downers"; "number" =1},
+        @{"tick" = 300; "enemies" = "downers"; "number" =1},
+        @{"tick" = 310; "enemies" = "downers"; "number" =5},
+        @{"tick" = 320; "enemies" = "downers"; "number" =1},
+        @{"tick" = 330; "enemies" = "downers"; "number" =1},
+        @{"tick" = 400; "enemies" = "downers"; "number" =5},
+        @{"tick" = 500; "enemies" = "downers"; "number" =1}
+    )
+}
+
+$hard1 =  @{
+    "Diffculty" = "hard";
+    "PlaneTypes" = ("downers");
+    "CompleteAt" = 500;
+    "level" = (
+        @{"tick" = 10; "enemies" = "downers"; "number" =2},
+        @{"tick" = 20; "enemies" = "downers"; "number" =5},
+        @{"tick" = 30; "enemies" = "downers"; "number" =3},
+        @{"tick" = 40; "enemies" = "downers"; "number" =2},
+        @{"tick" = 50; "enemies" = "downers"; "number" =3},
+        @{"tick" = 60; "enemies" = "downers"; "number" =3},
+        @{"tick" = 70; "enemies" = "downers"; "number" =2},
+        @{"tick" = 80; "enemies" = "downers"; "number" =2},
+        @{"tick" = 90; "enemies" = "downers"; "number" =2},
+        @{"tick" = 100; "enemies" = "downers"; "number" =2},
+        @{"tick" = 110; "enemies" = "downers"; "number" =2},
+        @{"tick" = 120; "enemies" = "downers"; "number" =2},
+        @{"tick" = 130; "enemies" = "downers"; "number" =3},
+        @{"tick" = 140; "enemies" = "downers"; "number" =2},
+        @{"tick" = 150; "enemies" = "downers"; "number" =2},
+        @{"tick" = 160; "enemies" = "downers"; "number" =2},
+        @{"tick" = 170; "enemies" = "downers"; "number" =2},
+        @{"tick" = 180; "enemies" = "downers"; "number" =5},
+        @{"tick" = 190; "enemies" = "downers"; "number" =2},
+        @{"tick" = 200; "enemies" = "downers"; "number" =2},
+        @{"tick" = 300; "enemies" = "downers"; "number" =2},
+        @{"tick" = 310; "enemies" = "downers"; "number" =5},
+        @{"tick" = 320; "enemies" = "downers"; "number" =2},
+        @{"tick" = 330; "enemies" = "downers"; "number" =2},
+        @{"tick" = 400; "enemies" = "downers"; "number" =5},
+        @{"tick" = 500; "enemies" = "downers"; "number" =2}
+    )
+}
 <#
 	Message: 00001001
 	Session ID: 1
