0f2b2e68079b1ca54b67d0108986272a274d5e0f
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-06-01 14:47:23 -0400

    Create DTCPP_functionality.test.py
    template for DTCPP_functionality_test_py

---
+++ Source/DTCPP.tests/DTCPP_functionality.test.py
@@ -0,0 +1,36 @@
+'''
+tests the file DTCPP_functioanlity
+'''
+import unittest
+
+class DTCPP_functioanlity_should(unittest.TestCase):
+    
+    def test_validate_request_should_return_true(self):
+        raise Exception('not built yet')
+    
+    def test_validate_request_should_return_false(self):
+        raise Exception('not built yet')
+    
+    def test_initialize_game_should_return_RECEIVE_SESSION_ID(self):
+        raise Exception('not built yet')
+    
+    def test_initialize_game_should_return_CANNOT_INITIZE_GAME(self):
+        raise Exception('not built yet')
+    
+    def test_select_level_should_return_RECEIVE_LEVEL(self):
+        raise Exception('not built yet')
+    
+    def test_select_level_should_return_ERROR(self):
+        raise Exception('not built yet')
+    
+    def test_select_level_should_return_ERROR_2(self):
+        raise Exception('not built yet')
+    
+    def test_end_game_should_return_OK(self):
+        raise Exception('not built yet')
+    
+    def test_end_should_return_ERROR(self):
+        raise Exception('not built yet')
+
+if __name__ == "__main__":
+    unittest.main()
