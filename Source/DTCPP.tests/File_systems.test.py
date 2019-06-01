c1cad78fe70ac671bf2c5037a5dbef5a607559b6
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-31 21:17:48 -0400

    Update File_systems.test.py
    created the definitions

--- Source/DTCPP.tests/File_systems.test.py
+++ Source/DTCPP.tests/File_systems.test.py
@@ -1,1 +1,20 @@
 import unittest
+import Classes.Message_ids
+import Classes.PDU
+
+class File_systems_should (unittest.TestCase):
+    def should_find_file(self):
+        return True
+    def should_not_find_file(self):
+        return False
+    def should_delete_file(self):
+        return True
+    def should_error_on_delete_file(self):
+        return True
+    def should_create_file_true(self):
+        return True
+    def should_createfile_false(self):
+        return True
+    def should_check_directory_true(self):
+        return True
+    def should_check_directory_false(self):
