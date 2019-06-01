bc846565e18c99622755b6f234c5103ee77901e9
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-06-01 18:32:12 -0400

    Update DTCPP_functionality.test.py
    test_initialize_game_should_return_RECEIVE_SESSION_ID created

--- Source/DTCPP.tests/DTCPP_functionality.test.py
+++ Source/DTCPP.tests/DTCPP_functionality.test.py
@@ -10,18 +10,23 @@ from Classes.Message_ids import Message_ids
 
 class DTCPP_functioanlity_should(unittest.TestCase):
     def setUp(self):
-        self.validate_good = PDU(Message_ids.INTIALIZE_GAME,1,bytes(56),None)
-
+        self.validate_good = {'Message' : Message_ids.INTIALIZE_GAME, 'SessionId': 1, 'Version' : bytes(56),'Data' : None}
+        self.validate_bad = {'bad' : 'This is will produce a false'}
+        self.initialize_game = {'Message' : Message_ids.INTIALIZE_GAME, 'SessionId': 1, 'Version' : bytes(56), 'Data' : None}
+    
     def test_validate_request_should_return_true(self):
         validate_good_is_good = validate_request(self.validate_good)
         self.assertTrue(validate_good_is_good)
     
     def test_validate_request_should_return_false(self):
-        raise Exception('not built yet')
+        validate_request_is_false = validate_request(self.validate_bad)
+        self.assertFalse(validate_request_is_false)
     
     def test_initialize_game_should_return_RECEIVE_SESSION_ID(self):
-        raise Exception('not built yet')
-    
+        initialize_game_response = initialize_game(self.initialize_game)
+        is_the_response_RECEIVED = initialize_game_response.message == Message_ids.RECEIVE_SESSION_ID
+        self.assertTrue(is_the_response_RECEIVED)
+
     def test_initialize_game_should_return_CANNOT_INITIZE_GAME(self):
         raise Exception('not built yet')
     
