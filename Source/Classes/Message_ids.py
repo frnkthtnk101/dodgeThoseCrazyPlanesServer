c2735e9b8ad78102ec03d7aaf82ee6b8e207626d
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-06-02 11:20:02 -0400

    Update Message_ids.py
    properly did the byte converson

--- Source/Classes/Message_ids.py
+++ Source/Classes/Message_ids.py
@@ -1,16 +1,16 @@
 from enum import Enum
 
 class Message_ids(Enum):
-    INTIALIZE_GAME = bytes(0)
-    CANNOT_INITIALIZE_GAME = bytes(1)
-    RECEIVE_SESSION_ID = bytes(2)
-    END_GAME = bytes(3)
-    QUIT_GAME = bytes(4)
-    OK = bytes(5)
-    SEND_SESSION_ID = bytes(6)
-    BAD_SESSION_ID = bytes(7)
-    GET_LEVEL = bytes(8)
-    RECEIVE_LEVEL = bytes(9)
-    BAD_LEVEL = bytes(10)
-    ERROR = bytes(11)
+    INTIALIZE_GAME = (0).to_bytes(1, byteorder='big')
+    CANNOT_INITIALIZE_GAME = (1).to_bytes(1, byteorder='big')
+    RECEIVE_SESSION_ID = (2).to_bytes(1, byteorder='big')
+    END_GAME = (3).to_bytes(1, byteorder='big')
+    QUIT_GAME = (4).to_bytes(1, byteorder='big')
+    OK = (5).to_bytes(1, byteorder='big')
+    SEND_SESSION_ID = (6).to_bytes(1, byteorder='big')
+    BAD_SESSION_ID = (7).to_bytes(1, byteorder='big')
+    GET_LEVEL = (8).to_bytes(1, byteorder='big')
+    RECEIVE_LEVEL = (9).to_bytes(1, byteorder='big')
+    BAD_LEVEL = (10).to_bytes(1, byteorder='big')
+    ERROR = (11).to_bytes(1, byteorder='big')
 
